dd8464a020baad3aa22ccd7710aca79b68209749
Tonosho <102292299+Tonosho@users.noreply.github.com>
2023-03-21 17:24:20 +0100

    cleaned app file + txt file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import { Pokeball } from "./Components/Pokeball";
 import "./App.css";
 
-// TO DO
-
-// IDEES
-// [] pagination du pokedex
-// [] pouvoir utiliser le clavier
-
-// FONCTIONNEL
-// [] corriger index quand on charge des nouveaux pokemon ?
-// [] transformer les filtres en boutons dans une modal
-
-// DESIGN
-// [] espacement v mobile
-// [] couleurs
-// [] afficher types sous fromes "d'étiquettes"
-
-// API LINK : "https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
-
 export const App = () => {
   return (
     <div className="App">
